65c976a12bca9705762ba93660f892136ad3eb17
anshu.wang.work@gmail.com <anshu.wang.work@gmail.com>
2017-04-07 15:30:08 +0800

    Signed-off-by: anshu.wang.work@gmail.com <anshu.wang.work@gmail.com>

--- writting/项目笔记/日期/日期.groovy
+++ writting/项目笔记/日期/日期.groovy
@@ -5,20 +5,18 @@ import java.util.Calendar;
 import java.util.Date;
 
 Date localDate=new Date();
-long serverTimeStamp=localDate.getTime()+TimeUnit.HOURS.toMillis(1);
+long serverTimeStamp=localDate.getTime()+TimeUnit.HOURS.toMillis(1);//东九区时间戳
 
-long diff=serverTimeStamp-localDate.getTime()
+long diff=serverTimeStamp-localDate.getTime()//东九区时间戳与本地相差
 
 TimeZone.getDefault()
 
-Date newServerDate=new Date(new Date().getTime()+diff)//服务器时间戳的本地时间
+Date newServerDate=new Date(new Date().getTime()+diff)//本地时区，服务器时间戳的本地时钟
 SimpleDateFormat serverFormat = new SimpleDateFormat("HH:mm");
 //serverFormat.setTimeZone(TimeZone.getTimeZone("GMT+9:00"));
 serverFormat.format(newServerDate)
-
+/*
 Date begin = new Date();
 begin.setHours(20);
 begin.setMinutes(53);
-
-begin
-newServerDate
+*/ 
